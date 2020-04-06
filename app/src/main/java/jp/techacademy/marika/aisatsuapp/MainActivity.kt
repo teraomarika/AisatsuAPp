330f3b2991269bdd5e3a3ccf55accde1616496ab
teraomarika <marika.terao.moapp@gmail.com>
2020-04-06 19:37:17 +0900

    textview1反映されない

--- app/src/main/java/jp/techacademy/marika/aisatsuapp/MainActivity.kt
+++ app/src/main/java/jp/techacademy/marika/aisatsuapp/MainActivity.kt
@@ -6,15 +6,18 @@ import androidx.appcompat.app.AppCompatActivity
 import android.os.Bundle
 import android.util.Log
 import android.view.View
+import android.widget.TextView
 import android.widget.TimePicker
 import kotlinx.android.synthetic.main.activity_main.*
 import java.time.OffsetDateTime
 
-class MainActivity : AppCompatActivity(),View.OnClickListener,TimePickerDialog.OnTimeSetListener, {
+class MainActivity : AppCompatActivity(),View.OnClickListener,TimePickerDialog.OnTimeSetListener{
 
     override fun onCreate(savedInstanceState: Bundle?) {
         super.onCreate(savedInstanceState)
         setContentView(R.layout.activity_main)
+        val messageView: TextView = findViewById(R.id.textView1)
+//        messageView.setText()
 
         button1.setOnClickListener(this)
 
@@ -31,21 +34,43 @@ class MainActivity : AppCompatActivity(),View.OnClickListener,TimePickerDialog.O
             this,
             TimePickerDialog.OnTimeSetListener { view, hour, minute ->
                 Log.d("aisatsu_PARTS", "$hour:$minute")
+//                onTimeSet(view: TimePicker?, hour; minute)
             },
             15, 0, true
         )
+        timePickerDialog.show()
 
-        override fun onTimeSet(view: TimePicker?, hourOfDay: Int, minute: Int) {
-            when(hourOfDay){
-                in 2..9 ->Log.d("aisatsu_PARTS", "おはよう")
-                in 10..17->Log.d("aisatsu_PARTS", "こんにちは")
-                else ->Log.d("aisatsu_PARTS", "こんばんは")}
+    }
+
+    override fun onTimeSet(view: TimePicker, hourOfDay: Int, minute: Int) {
+
+        var greeting:String ="初期"
+        when (hourOfDay) {
+            in 2..9 -> {Log.d("aisatsu_PARTS", "おはよう")
+                greeting=="おはよう"
+            }
+            in 10..17 -> {Log.d("aisatsu_PARTS", "こんにちは")
+                greeting=="こんにちは"}
+
+
+            else -> {Log.d("aisatsu_PARTS", "こんばんは")
+                greeting=="こんばんは"}
+
 
         }
-        timePickerDialog.show()
-    }
+//        textView1.text=greeting.
+//        listener?.onSelected(greeting)
+
+        val messageView: TextView = findViewById(R.id.textView1)
+        messageView.setText(greeting)
+
+
+
+
+
 
     }
+}
 
 
 
